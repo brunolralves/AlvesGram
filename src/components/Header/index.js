ca23d6118d632db5ba4b3932fff13e03df0e53a9
brunolralves <brunolralves@yahoo.com>
2021-10-27 21:32:09 -0300

    Implementados o Header e iniciada construção do feed de noticias

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,18 @@ export default function Header() {
  return (
    <View style={styles.header}>
      <TouchableOpacity>
-      <Text>Sou um botão</Text>
+      <Image 
+     source={require('../../img/logo.png')}
+     style={styles.logo}
+     />
      </TouchableOpacity>
+     <TouchableOpacity>
+      <Image 
+     source={require('../../img/send.png')}
+     style={styles.send}
+     />
+     </TouchableOpacity>
+     
    </View>
   );
 }
@@ -14,10 +24,21 @@ export default function Header() {
 const styles = StyleSheet.create({
   header:{
     height: 55,
-    backgroundColor:'black',
-    
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    borderBottomWidth:0.2,
+    elevation:1,
+    shadowColor:'gray'
+
   },
   logo:{
+    marginLeft:15,
+  },
+  send:{
+    height: 23,
+    width: 23,
+    marginRight:15,
 
   }
 });
---
+++ src/components/List/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View,Text,StyleSheet,ScrollView } from 'react-native';
+
+export default function List() {
+ return (
+   <ScrollView style={styles.list}>
+     <Text>Sou um feed</Text>
+   </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  list:{
+    
+
+  }
+});
